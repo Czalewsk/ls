2b18a113bc0bb1aaaa440dc5d839b147991be5e1
Dricc <czalewsk@e2r9p14.42.fr>
2017-09-17 19:36:21 +0200

    normd

--- ls_display_ext2.c
+++ ls_display_ext2.c
@@ -6,7 +6,7 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 17:55:44 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/09/17 19:19:18 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/09/17 19:36:00 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static void			ls_print_line(char *line, t_ls_ext *info, t_ls_info *file)
 	tmp = ft_itoa(ln->link);
 	ft_cpy_until_r(line, tmp, ' ', info->link);
 	free(tmp);
-	ft_cpy_until_r(line, ln->owner, ' ', info->user+ 1);
+	ft_cpy_until_r(line, ln->owner, ' ', info->user + 1);
 	ft_strcat(line, "  ");
 	ft_cpy_until(line, ln->group, ' ', info->group + 1);
 	tmp = ft_itoa(ln->size);
